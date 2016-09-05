67cd758b867f806b2228bbbacd31c1b50249b386
William Reilly <wreilly2001@gmail.com>
2016-09-05 05:16:26 -0400

    The Todo Reducer and its Test

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,4 +1,7 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
 
+// ------------------------------
 // Search text
 export var searchTextReducer = (state = '', action) => {
 
@@ -24,13 +27,14 @@ export var searchTextReducer = (state = '', action) => {
 };
 
 
+// ------------------------------
 // showCompletedReducer default: false; TOGGLE_SHOW_COMPLETED
 // default
 export var showCompletedReducer = (state = false, action) => {
   switch (action.type) {
     case 'TOGGLE_SHOW_COMPLETED':
       return !state; // All you need.
-// I was missing something - namely that the false cames in on state=
+// I was missing something - namely that the false came in on state=
       var oppositeCompletedState = true; // just make this thing a boolean
       if (action.completed) { // if it's true make it false
         oppositeCompletedState = false;
@@ -38,6 +42,132 @@ export var showCompletedReducer = (state = false, action) => {
         oppositeCompletedState = true;
       }
       return  oppositeCompletedState; // reverse the Boolean
+    default:
+      return state;
+  }
+};
+
+
+// ------------------------------
+// todos plural! array List of todos
+export var todosReducer = ( state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: uuid(),
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined,
+        }
+      ];
+
+    // case for TOGGLE_TODO
+    // match item of action.id e.g. 1
+    // modify - set completed ! oppositeC
+    // set commpleted at timestamp or clear
+    // HMM. STATE is that passed-in array, of todos
+    case 'TOGGLE_TODO':
+      var todosHere = [
+        ...state,
+      ]
+      return todosHere.map( (todoHere) => {
+        if (todoHere.id === action.id) {
+          var wasCompletedTF = todoHere.completed;
+          // var flipWasCompletedTF = !wasCompletedTF;
+          // todoHere.completed = !todoHere.completed; // <<< NO!
+          // if (todoHere.completedAt) {
+          //   // Held some value (timestamp)
+          //   todoHere.completedAt = undefined; // clear it out // <<< NO!
+          // } else {
+          //   todoHere.completedAt = moment().unix(); // <<< NO!
+          // }
+          return {
+              ...todoHere,
+              completed: !wasCompletedTF,
+              completedAt: !wasCompletedTF ? moment().unix() : undefined ,
+// NOPE:              completedAt: completed ? moment().unix() : undefined , // << ReferenceError: completed is not defined
+
+// WORKS:
+              // completed: flipWasCompletedTF,
+              // completedAt: flipWasCompletedTF ? moment().unix() : undefined ,
+          };
+        }
+      });
+
+
+/* ********* PERFESSER CODE *********** */
+/*
+      return state.map( (todo) => {
+        if (todo.id === action.id) {
+          var nextCompleted = !todo.completed;
+          // map lets you modify; return array element, which here is an object:
+          return {
+            ...todo,
+            completed: nextCompleted,
+            completedAt: nextCompleted ? moment().unix() : undefined ,
+          };
+        }
+      });
+*/
+/* ********* PERFESSER CODE *********** */
+
+/* WR__ code, WR__ learning:
+
+All right:
+1) Yes, we wanted a MAP, not a stinking FOREACH. Good.
+2) Yes, used an IF statement to find the one item to toggle. Good.
+3) But, inside a MAP, you do NOT update/modify/set properties directly
+onto the object/array element/property passed to it. (That's what I did.)
+No!
+You instead are to RETURN to the array the kind of thing you got
+from it (e.g. here an object, holding Todo information).
+The values in the object you return MAY be updated/modified. Bon.
+
+      // find todo to toggle, based on id:
+      var todosHere = [
+        ...state // spread, get the passed-in array contents inside our array here
+      ];
+      // Use MAP, not FOREACH:
+      todosHere.map( (todoHere) => {
+        if (todoHere.id === action.id) {
+          // for the one toggled:
+            // completed to !completed
+            // completedAt to timestamp, OR clear
+          todoHere.completed = !todoHere.completed; // <<< NO!
+          if (todoHere.completedAt) {
+            // Held some value (timestamp)
+            todoHere.completedAt = undefined; // clear it out // <<< NO!
+          } else {
+            todoHere.completedAt = moment().unix(); // <<< NO!
+          }
+          return todoHere; // <<< You THINK you're "returning", but doesn't work this way.
+          // You don't "return" the variable you get.
+          // You make something new, return that back in its stead.
+        } else {
+          // wasn't toggled; return it as-is:
+          return todoHere; // <<< Sort of superfluous; MAP will just
+          //  "return" it if you do nothing to it, here in the MAP function.
+        }
+      });
+*/ // / "Learning"
+
+      // NAH:
+      /*
+      todosHere.forEach( function (todoHere) {
+        if (todoHere.id === action.id) {
+          // that's the one
+
+        } else {
+          // just put back the unmodified ones
+
+        }
+      }
+      */
+      // No. The MAP already takes care of returning the array, etc.
+      // return todosHere; // ? <<< No. Not needed.
     default:
       return state;
   }
--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -40,4 +40,53 @@ describe('Reducers',  () => {
     });
   });
 
-});
+  describe('todosReducer', () => {
+    it('should add new todo (singular)', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk that dog, once again',
+      };
+      var res = reducers.todosReducer( df([]), df(action));
+      expect(res.length).toEqual(1); // added 1 todo to array
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    // case for TOGGLE_TODO
+    // match item of action.id e.g. 1
+    // modify - set completed ! oppositeC
+    // set commpleted at timestamp or clear
+
+// pass array as default; toggle action; check has completed opposite
+// todos array w realistic todo item
+//action with that id #
+// assert completedFlipped
+
+    it('should toggle a single todo  --- completed or not; timestamp or clear', () => {
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 1,
+      };
+      var todosTest = [
+        {
+          id: 1,
+          text: "Toggle this one!",
+          completed: true,
+          completedAt: 2000, // 2,000 seconds into 1970
+          createdAt: 1000, // 1,000 seconds into 1970
+        },
+        {
+          id: 2,
+          text: "Leave this one untouched, kids!",
+          completed: false,
+          completedAt: undefined, // never got to this, back in 1970
+          createdAt: 3000, // 3,000 seconds into 1970
+        },
+      ];
+      var res = reducers.todosReducer(df(todosTest), df(action));
+      expect(res[0].completed).toBe(false);
+      expect(res[0].completedAt).toBe(undefined);
+    });
+
+  }); // todosReducer
+
+}); // Reducers
